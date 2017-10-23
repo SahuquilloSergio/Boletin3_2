ad1095351260149e92003eff99f3124b9fdd11bd
Sauky <Sauky@DESKTOP-2HUBGV7>
2017-10-23 19:43:08 +0200

    Commit Inicial

---
+++ src/boletin3_2/Boletin3_2.java
@@ -0,0 +1,13 @@
+package boletin3_2;
+import java.util.Scanner;
+public class Boletin3_2 {
+
+    public static void main(String[] args) {
+       Satelite Satelite1 = new Satelite();
+           Scanner ler = new Scanner(System.in);
+           Satelite1.verPosicion();
+           
+           
+    }
+    
+}
